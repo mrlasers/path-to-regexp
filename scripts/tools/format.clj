12a0a22c948cb12df14a811d901bdffe5991a34d
Maksim Ryzhikov <rv.maksim@gmail.com>
2022-04-03 20:58:22 +0200

    chore: use check as default for format

--- scripts/tools/format.clj
+++ scripts/tools/format.clj
@@ -3,9 +3,9 @@
    [cljfmt.main :as cljfmt]))
 
 (defn -main [& args]
-  (let [command (first args)
+  (let [command (keyword (or (first args) "check"))
         paths ["src" "test" "scripts"]
         options (cljfmt/merge-default-options
                   (read-string (slurp ".cljfmt.clj")))]
-    (when (= command "check") (cljfmt/check paths options))
-    (when (= command "fix") (cljfmt/fix paths options))))
+    (when (= command :check) (cljfmt/check paths options))
+    (when (= command :fix) (cljfmt/fix paths options))))
